228d01d49cd2b4dfe4eaf7a1e46c64ddcb5c762f
Yan Soares <yts.1995@gmail.com>
2019-08-02 14:19:24 -0300

    Update UserController.js

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup';
 import User from '../models/User';
+import File from '../models/File';
 
 class UserController {
     async store(req, res) {
@@ -71,15 +72,35 @@ class UserController {
         if (oldPassword && !(await user.checkPassword(oldPassword))) {
             return res.status(401).json({ error: 'Password does not match' });
         }
+        
+        await user.update(req.body);
 
-        const { id, name, provider } = await user.update(req.body);
+        const { id, name, avatar } = await User.findByPk(req.userId, {
+            include: [
+                {
+                    model: File,
+                    as: 'avatar',
+                    attributes: ['id', 'path', 'url'],
+                },
+            ],
+        });
+
+        return res.json({
+            id,
+            name,
+            email,
+            avatar,
+        });
+  }
+
+        /*const { id, name, provider } = await user.update(req.body);
 
         return res.json({
             id,
             name,
             email,
             provider,
-        });
+        });*/
     }
 }
 
